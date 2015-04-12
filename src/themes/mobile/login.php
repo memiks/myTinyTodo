58e673955af21cc060addb9d1348eb6c8eb5c8bd
Zdenko Podobný <zdenop@gmail.com>
2015-04-12 15:22:49 +0200

    fix mobile login, improve default version

--- src/themes/mobile/login.php
+++ src/themes/mobile/login.php
@@ -1,5 +1,5 @@
 <?php
-$hash = sha1(Config::get('password') . Config::get('username') . @$_SERVER['SERVER_ADDR'] . @$_SERVER['HTTP_HOST']);
+$hash = sha1(Config::get('password') . Config::get('username') . $_SERVER['SERVER_ADDR'] . $_SERVER['HTTP_HOST']);
 if (Config::get('password') == '' || (isset($_COOKIE['MTTAUTH']) && $_COOKIE['MTTAUTH'] == $hash) || (isset($_POST['pasword']) && isset($_POST['user']) && $_POST['pasword'] == Config::get('password') && $_POST['user'] == Config::get('username'))) {
     session_regenerate_id(1);
     $_SESSION['logged'] = 1;
@@ -29,44 +29,40 @@ if (Config::get('password') == '' || (isset($_COOKIE['MTTAUTH']) && $_COOKIE['MT
           href="<?php mttinfo('template_url'); ?>pda.css?v=<?php echo Config::get('version'); ?>"
           media="all"/>
 
-    <style>body {
-            margin-top: 0;
-            margin-right: auto;
-            margin-left: auto;
-            margin-bottom: 0;
-            width: 960px;
-            padding-top: 50px
-        }
+    <script type="text/javascript">
+        $().ready(function () {
+            $('body').width(screen.width);
+            $(window).resize(function () {
+                $('body').width(screen.width);
+            });
+    </script>
 
-        .login-form {
-            margin: 50px auto;
-        }</style>
 </head>
 
 <body>
-<div class="login-form">
-    <h2><?php mttinfo('title'); ?></h2>
 
-    <form accept-charset="UTF-8" id="loginForm" method='POST' action='<?php echo $_SERVER['REQUEST_URI']; ?>'>
-        <input type="text" id="user" name="user" placeholder="<?php _e('set_user'); ?>"/>
-        <input type='password' id='password' name='pasword' placeholder="<?php _e('password'); ?>"/>
-         <span>
-            <input type="checkbox" name="checkbox" value='1'>
-            <label for="checkbox"><?php _e('stay_loggedin'); ?></label>
-        </span>
-        <input type='submit' value='<?php _e('btn_login'); ?>'/>
-    </form>
-</div>
-<div id="page_ajax" style="display:none"></div>
-
-</div>
-<div id="space"></div>
-</div>
-
-<div id="footer">
-    <div id="footer_content">Powered by <strong><a
-                href="http://www.mytinytodo.net/">myTinyTodo</a></strong> <?php echo Config::get('version'); ?> </div>
-</div>
+<div id="wrapper">
+    <div id="container">
+        <div id="mtt_body">
+            <div id="BAR">
+                <h2><?php mttinfo('title'); ?></h2>
+            </div>
+            <form accept-charset="UTF-8" id="loginForm" method='POST' action='<?php echo $_SERVER['REQUEST_URI']; ?>'>
+                <input type="text" id="user" name="user" placeholder="<?php _e('set_user'); ?>"/>
+                <input type='password' id='password' name='pasword' placeholder="<?php _e('password'); ?>"/>
+                 <span>
+                    <input type="checkbox" name="checkbox" value='1'>
+                    <label for="checkbox"><?php _e('stay_loggedin'); ?></label>
+                </span>
+                <input type='submit' value='<?php _e('btn_login'); ?>'/>
+            </form>
+        </div>
+    </div>
+    <div id="footer">
+        <div id="footer_content">Powered by <strong>
+                <a href="http://www.mytinytodo.net/">myTinyTodo</a></strong> <?php echo Config::get('version'); ?>
+        </div>
+    </div>
 </div>
 </body>
 </html>
